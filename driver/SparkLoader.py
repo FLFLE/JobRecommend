98ebcfb8673eb8e938d007594f35525dbf175d08
曾凡宇 <1792236446@qq.com>
2022-03-03 15:09:33 +0800

    SparkLoader工具

---
+++ driver/SparkLoader.py
@@ -0,0 +1,11 @@
+from driver.SparkSessionBase import SparkSessionBase
+
+
+class SparkLoader(SparkSessionBase):
+    SPARK_APP_NAME = 'Tag'
+    SPARK_URL = 'local'
+    ENABLE_HIVE_SUPPORT = True
+
+    def __init__(self):
+        self.spark = self._create_spark_session()
+
