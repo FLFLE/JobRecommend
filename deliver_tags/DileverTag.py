bbbc18aa019b3e8729d5677d23838be25fcb29dd
Clint2019 <1126995390@qq.com>
2022-03-04 09:55:21 +0800

    用户主题添加标签、投递主题添加标签

---
+++ deliver_tags/DileverTag.py
@@ -0,0 +1,36 @@
+#coding=utf-8
+from pyspark.sql.functions import *
+from driver.SparkLoader import SparkLoader
+
+spark=SparkLoader().spark
+hcx = SparkLoader().hcx
+account_df = hcx.table('talents.ods_account')
+position_df = hcx.table('talents.ods_position')
+hcx.table('talents.ods_company')
+deliver_df=hcx.table('talents.ods_deliver')
+
+#添加各期望城市投递数标签
+cleaned_account=account_df.dropna('any',col('expectcity'))
+cleaned_account.join(position_df,account_df['expectcity']==position_df['city'])\
+    .join(deliver_df,position_df['id']==deliver_df['position_id'],'right_outer') \
+    .select(account_df['expectcity'],deliver_df['position_id']) \
+    .groupby(account_df['expectcity']).agg(count(deliver_df['position_id']).alias('count'))
+
+
+#添加是否投递占比标签
+# deliver_num=deliver_df.join(account_df,deliver_df['account_id']==account_df['id']).select('account_id').groupby('account_id').count()
+# deliver_partition_df=deliver_df.dropna().groupBy('id').agg((deliver_num.alias("是"))
+#添加每日投递数量标签
+# deliver_df.select(dayofyear('deliver_time').alias('day')).groupBy('day')\
+#     .agg(count('day')).show()
+#投递用户Top10
+# deliver_df.select(col('account_id').alias('投递用户')) \
+#             .groupBy('投递用户').agg(count('投递用户').alias('count')).orderBy(col('count').desc()).show(10,truncate=False)
+# highesteducation_df=df.select(col('highesteducation').alias('学历信息')) \
+#             .groupBy('学历信息').agg(count('学历信息').alias('count')).where('count > 10')
+#投递学历层次占比
+deliver_df.join(account_df,deliver_df['account_id']==account_df['id'])\
+    .select(account_df['highesteducation']) \
+    .groupBy('highesteducation').agg(count('highesteducation').alias('count')).where('count > 10').show()
+
+
---
+++ user_tags/UserTag.py
@@ -0,0 +1,60 @@
+#coding=utf-8
+from typing import List
+
+from pyspark.sql import HiveContext
+from pyspark.sql.types import *
+from pyspark.sql.functions import *
+from driver.SparkSessionBase import SparkSessionBase
+
+
+class UserGenderTag(SparkSessionBase):
+    SPARK_APP_NAME = 'UserGenderTag'
+    SPARK_URL = 'local'
+    ENABLE_HIVE_SUPPORT = True
+
+    def __init__(self):
+        self.spark = self._create_spark_session()
+
+    def process(self):
+        hcx = HiveContext(self.spark.sparkContext)
+        df = hcx.table('talents.ods_account')
+        hcx.table('talents.ods_position')
+        hcx.table('talents.ods_company')
+        hcx.table('talents.ods_deliver')
+        # df.select('id',when(col('sex') == '男', '男').when(col('sex') == '女', '女').otherwise('未知').alias('性别')
+        #           ).groupby('性别').agg(count('性别').alias('count')).show()
+
+        # df.select('id', when(col('age') >= 63, '50后').when(col('age') >= 53, '60后') \
+        #         .when(col('age') >= 43, '70后').when(col('age') >= 33, '80后') \
+        #         .when(col('age') >= 23, '90后').otherwise('00后').alias('年龄段')) \
+        #     .groupBy('年龄段').count().orderBy(col('年龄段').desc()).show()
+
+        # df.select(col('expectcity').alias('期望城市')) \
+        #     .groupBy('期望城市').agg(count('期望城市').alias('count')).show()
+
+        # df.select(col('status').alias('在职状态'))\
+        #     .groupby('在职状态').agg(count('在职状态').alias('count')).where('count >=10').show()
+
+        # df.select(col('expectpositionname').alias('期望职位')) \
+        #     .groupBy('期望职位').agg(count('期望职位').alias('count')).where('count > 10').show()
+
+        # df.select(col('is_famous_enterprise').alias('是否名企')) \
+        #     .groupBy('是否名企').agg(count('是否名企').alias('count')).where('count > 10').show()
+
+        df.dropna('any').select(col('work_year').alias('工作年限')) \
+            .groupBy('工作年限').agg(count('工作年限').alias('count')).where('count > 10').show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    UserGenderTag().process()
