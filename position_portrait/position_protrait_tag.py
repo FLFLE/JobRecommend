3a8849a95e4a5a1ad8028c9022d92192e2856582
曾凡宇 <1792236446@qq.com>
2022-03-07 14:16:45 +0800

    职位画像的基本标签完善

--- position_portrait/position_protrait_tag.py
+++ position_portrait/position_protrait_tag.py
@@ -13,39 +13,59 @@ position_df = hcx.table('talents.ods_position')
 company_df = hcx.table('talents.ods_company')
 deliver_df = hcx.table('talents.ods_deliver')
 
-# 添加职位标签
-insert_position_id = 1007336
 
 # 职位画像城市标签
-position_city = position_df.select('city').where(col('id').like('{}'.format(insert_position_id)))
-position_city = position_city.collect()
-position_city = position_city[0][0]
-print(position_city)
+def get_position_city(position_id):
+    position_city = position_df.select('city').where(f'id == {position_id}')
+    position_city = position_city.collect()
+    position_city = position_city[0][0]
+    return position_city
 
-#职位画像薪资标签
-temp_df = position_df.withColumn('salaryrange', concat_ws('-', col('salarymin'), col('salarymax')))
-position_salary = temp_df.select('salaryrange').where(col('id').like('{}'.format(insert_position_id)))
-position_salary = position_salary.collect()
-position_salary = position_salary[0][0] + 'k'
-print(position_salary)
+
+# 职位画像薪资标签
+def get_position_salary(position_id):
+    tmp_df = position_df.withColumn('salaryrange', concat_ws('-', col('salarymin'), col('salarymax')))
+    position_salary = tmp_df.select('salaryrange').where(f'id == {position_id}')
+    position_salary = position_salary.collect()
+    position_salary = position_salary[0][0] + 'k'
+    return position_salary
+
 
 # 职位画像工作年限标签
-position_workyear = position_df.select('workyear').where(col('id').like('{}'.format(insert_position_id)))
-position_workyear = position_workyear.collect()
-position_workyear = position_workyear[0][0]
-print(position_workyear)
+def get_position_workyear(position_id):
+    position_workyear = position_df.select('workyear').where(f'id == {position_id}')
+    position_workyear = position_workyear.collect()
+    position_workyear = position_workyear[0][0]
+    return position_workyear
 
-# #职位画像工作类型标签
-temp_df = position_df.withColumn('positioncategory', concat(col('positionfirstcategory'), col('positionsecondcategory'),
-                                                            col('positionthirdcategory')))
-position_category = temp_df.select('positioncategory').where(col('id').like('{}'.format(insert_position_id)))
-position_category = position_category.collect()
-position_category = position_category[0][0]
-print(position_category)
 
-#
+# 职位画像工作类型标签
+def get_position_category(position_id):
+    temp_df = position_df.withColumn('positioncategory',
+                                     concat(col('positionfirstcategory'), col('positionsecondcategory'),
+                                            col('positionthirdcategory')))
+    position_category = temp_df.select('positioncategory').where(f'id == {position_id}')
+    position_category = position_category.collect()
+    position_category = position_category[0][0]
+    return position_category
+
+
 # 职位画像学历标签
-position_education = position_df.select('education').where(col('id').like('{}'.format(insert_position_id)))
-position_education = position_education.collect()
-position_education = position_education[0][0]
-print(position_education)
+def get_position_education(position_id):
+    position_education = position_df.select('education').where(f'id == {position_id}')
+    position_education = position_education.collect()
+    position_education = position_education[0][0]
+    return position_education
+
+
+class Position():
+    def __init__(self, position_id):
+        self.position_id = position_id
+
+    def get_position_protrait(self):
+        position_city = get_position_city(self.position_id)
+        position_salary = get_position_salary(self.position_id)
+        position_workyear = get_position_workyear(self.position_id)
+        position_category = get_position_category(self.position_id)
+        position_education = get_position_education(self.position_id)
+        return position_city, position_salary, position_workyear, position_category, position_education
